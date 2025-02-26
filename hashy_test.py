fc7c1cd7adb4cf8584578c3c5006f212b6b8f81b
Martina Pauer <martinapauer38@gmail.com>
2025-02-26 17:04:03 -0300

    correción prueba de integracion
    Hago mas justas las comparaciones porque hashy usa hash en minusculas y hasher en mayusculas

--- hashy_test.py
+++ hashy_test.py
@@ -7,12 +7,12 @@ import hashy
 # Uso formato nuevo de hasher con argumentos multilpes: ./hashy_test.py hash_1 hash_2 hash_n
 for hash_n in range(1, int(sys.argv.__len__())):
     # Pruebo archivo valido
-    if hashy.archivo_valido(hash_n, 'resultado_4.txt'):
+    if hashy.archivo_valido(sys.argv[int(hash_n)].__str__().lower().__str__(), 'resultado_4.txt'):
         print(f'\nEl hash "{sys.argv[int(hash_n)].__str__()}" es valido para "resultado_4.txt".\n')
     else:
         print(f'\nEl hash "{sys.argv[int(hash_n)].__str__()}" no verifica para "resultado_4.txt".\n')
     # Pruebo lista valida
-    if hashy.lista_valida(hash_n, [1, 3, 5]):
+    if hashy.lista_valida(sys.argv[int(hash_n)].__str__().lower().__str__(), [1, 3, 5]):
         # Pruebo hash de lista
         print(f'\n\nLa lista [1, 3, 5] es valida y tiene hash: {hahsy.hash_lista([1, 2, 3]).__str__()}.\n')
     else:
