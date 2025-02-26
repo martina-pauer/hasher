fa3eaddfa6ad17ba6899eaeef9db182ec46bec3e
Martina Pauer <martinapauer38@gmail.com>
2025-02-26 17:56:56 -0300

    correcion
    mas uso de hashy para codigo mas mantenible y con futura consistencia con hashlib

--- hasher.py
+++ hasher.py
@@ -2,32 +2,15 @@
 
 # Cifra un documento que reciba como parametros en hashes hexadecimales
 
-import sys
-
-# Hago as
+import sys, hashy
+# Argumento Multiple en comandos ./hasher.py arch_1 arch_2 arch_3 arch_n
 for archivo_n in range(1, int(sys.argv.__len__())):
-# Copio todo y encripto después en un hash único, eliminar cuanto antes la copia por seguridad luego de escribir al archivo
-    lectura = open(sys.argv[int(archivo_n)].__str__(), 'r')
-
-    copia = ''
-# Cifro las lineas con hashes positivos en hexadecimal
-    for linea in lectura.readlines():
-        '''
-            Aunque esta forma es lenta y riesgosa por existencia de variable copia en el tiempo, 
-            es la única que encuentro para concentrar todo el contenido en una variable para
-            luego cifrar.
-        '''
-        # Aprovecho ya para ir cifrando para lograr mas seguridad al menos en memoria
-        copia += hex((linea.__hash__() - 2).__abs__())
-    # Ya cierro la lectura del archivo y hasheo los hashes de las lineas por seguridad en memoria
-    lectura.close()
-    del lectura
-# Hago así para aumentar complejidad y asimetría del cifrado
-    copia = hex(copia.__hash__().__abs__() * 512).upper().replace("X", "x")
+# Hago uso de hashy para obtener un hash mas consistente y hacer uso de un módulo reutilizable para estandarizar 
+    copia = hashy.hash_archivo(sys.argv[int(archivo_n.__str__())]).upper().replace("X", "x")
 # Sobre escribo el archivo con la copia cifrada para que pierda lo anterior (debe haber copia de archivo en unidad externa por seguridad)
     cifrar = open(sys.argv[int(archivo_n)].__str__(), 'w')
 # Como recibo el nombre de afuera debo transformarlo en cadena para evitar inyecciones de código por parametro mal ingresado
-    cifrar.write(copia + '\n')
+    cifrar.write(copia.__str__() + '\n')
 # Cierro para evitar memory leaks y accesos no deseados por no liberar el archivo de la memoria
     cifrar.close()
 # Ya no necesito la referencia al archivo así que la elimino de la memoria por seguridad
